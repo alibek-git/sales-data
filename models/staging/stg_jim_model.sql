5cf4a61cf13a08eccbe881027230a20d5e5e724b
Alibek <alibek1991@gmail.com>
2023-07-04 06:19:49 +0000

    Modify staging models to add time difference between joined and event timestamp

--- models/staging/stg_jim_model.sql
+++ models/staging/stg_jim_model.sql
@@ -1,2 +1,4 @@
-SELECT *, EXTRACT(DAY FROM CURRENT_DATE - DATE(date_joined)) AS days_with_company
+SELECT *, 
+        EXTRACT(DAY FROM CURRENT_DATE - DATE(date_joined)) AS days_with_company,
+        TIMESTAMP_DIFF(timestamp, TIMESTAMP(date_joined), DAY) AS time_diff_event_and_joined
 FROM `dbt_azhubekov.stg_jim_upd`
