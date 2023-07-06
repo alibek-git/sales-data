fedecd6ae80976f2187f28cf836f3af1716a6ef9
Alibek <83647897+alibek-git@users.noreply.github.com>
2023-07-06 17:35:42 +0600

    Update stg_sales_data.sql

--- models/staging/stg_sales_data.sql
+++ models/staging/stg_sales_data.sql
@@ -1,2 +1,2 @@
-SELECT * -- selecting all 6 columns, activity_type, timestamp, user_id, description, salesperson, and date_joined for the salesperson named Phyllis
+SELECT * -- selecting all 6 columns, activity_type, timestamp, user_id, description, salesperson, and date_joined for all salespeople
 FROM `dbt_azhubekov.stg_sales`
